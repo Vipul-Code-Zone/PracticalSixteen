edab53a1a72dcf06fb61eedc53a34d18bbde536f
Vipul1432 <vipul.v@simformsolutions.com>
2023-07-14 10:21:37 +0530

    Add project files.

---
+++ PracticalSixteen/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticalSixteen.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : Controller
+    {
+        private ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Looging Hello World");
+            _logger.LogWarning("Hello World from warning");
+            _logger.LogCritical("Hello World from critical");
+            return Ok("Hello World");
+        }
+    }
+}
---
+++ PracticalSixteen/Program.cs
@@ -0,0 +1,31 @@
+namespace PracticalSixteen
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+            // app.UseHttpLogging();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Logger.LogInformation("Adding Routes");
+            app.MapGet("/", () => "Hello World!");
+            app.Logger.LogInformation("Starting the app");
+            app.Run();
+        }
+    }
+}
